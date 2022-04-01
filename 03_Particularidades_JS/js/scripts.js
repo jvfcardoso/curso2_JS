91f18328944830f241f35276339c53b7401d79eb
jvfcardoso <joaovictor_fc@outlook.com>
2022-04-01 10:44:12 -0300

    First commit

---
+++ 03_Particularidades_JS/js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+var a = 1;
+
+a = "Teste";
+
+console.log(a)
+
+// Este é um comentário
+
+/*
+Este
+é
+um
+comentário
+de
+múltiplas
+linhas
+*/
