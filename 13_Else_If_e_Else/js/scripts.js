83698db80bc73e092463dea37706887eee2475c7
jvfcardoso <joaovictor_fc@outlook.com>
2022-04-22 09:47:47 -0300

    Four commmit

---
+++ 13_Else_If_e_Else/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Xavier";
+
+if(nome == "Pedro"){
+    console.log("O nome dele é Pedro");
+}else if(nome == "João"){
+    console.log("O nome é João");
+}else if(nome == "Xavier"){
+    console.log("O nome é Xavier");
+}else{
+    console.log("Ele possui outro nome");
+}
+
+var idade = 19;
+
+if(idade > 20){
+    console.log("Ele pode entra na festa");
+}else if(idade >= 18){
+    console.log("Ele só pode entrar com autorização!");
+}
+
+if(nome == "João"){
+    console.log("Teste");
+}else{
+    console.log("Testando");
+}
---
+++ 17_Operador_logico_NOT/js/scripts.js
@@ -0,0 +1,13 @@
+if(!true){
+    console.log("Passou");
+}
+
+if(!false){
+    console.log("Passou")
+}
+
+var nome = "João";
+
+if(!(nome == "Pedro")){
+    console.log("Ok");
+}
