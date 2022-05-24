aecd287e28eb30bb3435b994a09350c4f8e14eda
jvfcardoso <joaovictor_fc@outlook.com>
2022-05-24 10:55:16 -0300

    Eighth commit

---
+++ 26_metodos_numericos/js/scripts.js
@@ -0,0 +1,26 @@
+// parseFloat 10.0
+
+console.log(parseFloat('12.999'));
+console.log(Number.parseFloat('12.999'));
+
+// parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(19.80));
+
+// toFixed
+
+console.log(22.55432.toFixed(2));
+
+// isNaN
+
+console.log(isNaN("Teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.7976931348623157e+308);
